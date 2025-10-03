eb2876636674b2d17db239991d274180dac1e4d0
John S. Urban <urbanjost@comcast.net>
2025-10-02 21:51:12 -0400

    examples

--- example/demo_M_unicode.f90
+++ example/demo_M_unicode.f90
@@ -1,38 +1,45 @@
       program demo_M_unicode
       use,intrinsic :: iso_fortran_env, only : stdout=>output_unit
-      use M_unicode,only : TOKENIZE, REPLACE, CHARACTER, UPPER, LOWER
+      use M_unicode,only : TOKENIZE, REPLACE, CHARACTER, UPPER, LOWER, LEN
       use M_unicode,only : unicode_type, assignment(=), operator(//)
-      use M_unicode,only : read(formatted), write(formatted)
       use M_unicode,only : ut => unicode_type, ch => character
-      type(unicode_type)             :: string, numeric, uppercase, lowercase
+      use M_unicode,only : read(formatted), write(formatted)
+      type(unicode_type)             :: string
+      type(unicode_type)             :: numeric, uppercase, lowercase
       type(unicode_type),allocatable :: array(:)
-      character(len=*),parameter     :: gen='(g0)'
+      character(len=*),parameter     :: all='(g0)'
       character(len=*),parameter     :: uni='(DT)'
       uppercase='АБВГҐДЕЄЖЗИІЇЙКЛМНОПРСТУФХЦЧШЩЬЮЯ'
       lowercase='абвгґдеєжзиіїйклмнопрстуфхцчшщьюя'
       numeric='0123456789'
 
        string=uppercase//' '//numeric//' '//lowercase
-       
-       write(stdout,gen)'Original string:'
-       write(stdout,uni) string
-       write(stdout,gen)
-       write(stdout,gen)'convert to all uppercase:'
-       write(stdout,uni) UPPER(string)
-       write(stdout,gen)
-       write(stdout,gen)'convert to all lowercase:'
-       write(stdout,uni) LOWER(string)
-       write(stdout,gen)
-       write(stdout,gen)'tokenize on spaces:'
+
+       print all, 'Original string:'
+       print all, ch(string)
+       print all, 'length in bytes :',len(string%character())
+       print all, 'length in glyphs:',len(string)
+       print all
+
+       print all, 'convert to all uppercase:'
+       print uni, UPPER(string)
+       print all
+
+       print all, 'convert to all lowercase:'
+       print uni, LOWER(string)
+       print all
+
+       print all, 'tokenize on spaces ... '
        call TOKENIZE(string,ut(' '),array)
-       write(stdout,gen) character(array)
-       write(stdout,uni) array
-       write(stdout,gen)
-       write(stdout,gen)'case-insensitive replace:'
-       write(stdout,uni) REPLACE(string, &
+       print all, '... writing with A or G format:',character(array)
+       print uni, ut('... writing with DT format'),array
+       print all
+
+       print all, 'case-insensitive replace:'
+       print uni,  REPLACE(string, &
        & ut('клмнопрс'), &
        & ut('--------'), &
        & ignorecase=.true.)
-       write(stdout,gen)
+       print all
 
       end program demo_M_unicode
