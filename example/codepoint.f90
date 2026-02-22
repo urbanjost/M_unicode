af3b79ef0a9c3b00388ac07ab23758fb8972a847
John S. Urban <urbanjost@comcast.net>
2026-02-22 04:58:26 -0500

    Improve assignment
    Make lhs=rhs work with rhs an array of character variables but lhs
    a scalar or an array.
    
    > program test_for_iso_10646
    > use M_unicode
    > use iso_fortran_env, only : stdout => output_unit
    > implicit none
    > type(unicode_type) :: smiley
    > type(unicode_type),allocatable :: arr(:)
    > integer :: i
    >
    > smiley=['1😃','2😃','3😃','4😃']
    > write(stdout,'(a,*(:"[",a,"]"))')'four CHARACTER strings becomes one UNICODE_TYPE string ',smiley%character()
    >
    > arr=['1😃','2😃','3😃','4😃']
    > write(stdout,'(a,*(:"[",a,"]"))')'four CHARACTER strings becomes four UNICODE_TYPE string ',(arr(i)%character(),i=1,size(arr))
    >
    >end program test_for_iso_10646

--- example/codepoint.f90
+++ example/codepoint.f90
@@ -3,6 +3,8 @@ use M_unicode
 use iso_fortran_env, only : stdout => output_unit
 implicit none
 type(unicode_type) :: smiley
+type(unicode_type),allocatable :: arr(:)
+integer :: i
 
    smiley=[int(z'1F603')]
    write(stdout,*)'Smiling face with open mouth',smiley%character() ! 😃
@@ -11,13 +13,15 @@ type(unicode_type) :: smiley
    write(stdout,*)'one CHARACTER string to one UNICODE_TYPE string',smiley%character()
 
    smiley=['1😃','2😃','3😃','4😃']
+   write(stdout,'(a,*(:"[",a,"]"))')'four CHARACTER strings becomes one UNICODE_TYPE string ',smiley%character()
 
-   write(stdout,*)'four CHARACTER strings becomes one UNICODE_TYPE string ',smiley%character()
+   arr=['1😃','2😃','3😃','4😃']
+   write(stdout,'(a,*(:"[",a,"]"))')'four CHARACTER strings becomes four UNICODE_TYPE string ',(arr(i)%character(),i=1,size(arr))
 
    smiley=128515
    write(stdout,*)'one codepoint ',smiley%character(),smiley%codepoint()
 
    smiley=[128515,128515]
-   write(stdout,*)'array of codepoint ',smiley%character(),smiley%codepoint()
+   write(stdout,*)'arr of codepoint ',smiley%character(),smiley%codepoint()
 
 end program test_for_iso_10646
