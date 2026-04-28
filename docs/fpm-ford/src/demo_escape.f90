3c0fc86ffe6ff6d8ae2193eb111ff69f53b8d596
urbanjost <urbanjost@comcast.net>
2026-04-28 11:20:03 +0000

    Deploy FORD docs

--- docs/fpm-ford/src/demo_escape.f90
+++ docs/fpm-ford/src/demo_escape.f90
@@ -5,7 +5,7 @@
      use M_unicode,       only : assignment(=), trim
      implicit none
      type(ut),allocatable  :: poem(:)
-     type(ut)              :: test(5)
+     type(ut),allocatable  :: test(:)
      integer               :: i
         !
         ! “The Crow and the Fox” by Jean de la Fontaine
